8cd45f3d8971536d7128e13e447a21979c774d3d
neicv <neicv@mail.ru>
2016-09-12 04:15:59 +0300

    upd fix

--- scripts.php
+++ scripts.php
@@ -50,5 +50,25 @@ return [
      * Runs all updates that are newer than the current version.
      *
      */
+	 
+	'updates' => [
+
+        '0.1.5' => function ($app) {
+
+			$util = $app['db']->getUtility();
+
+			if ($util->tableExists('@search_keywords') === false) {
+				$util->createTable('@search_keywords', function ($table) {
+					$table->addColumn('id', 'integer', ['unsigned' => true, 'length' => 10, 'autoincrement' => true]);
+					$table->addColumn('word', 'string', ['length' => 255]);
+					$table->addColumn('ip', 'integer',['unsigned' => true, 'default' => 0]);
+					$table->addColumn('putdate', 'datetime', ['notnull' => false]);
+					$table->setPrimaryKey(['id']);
+					$table->addIndex(['word'], '@SEARCH_KEYWORDS_WORD');
+					$table->addIndex(['putdate'], '@SEARCH_KEYWORDS_PUTDATE');
+				});
+			}
+		}
+    ]
 
 ];
